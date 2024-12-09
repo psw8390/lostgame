a8d66c22943dd08a07bd4ceec0f676f141f15e52
psw8390 <psw8390@gmail.com>
2024-12-09 09:49:13 +0900

    feat(router): add redirection from '/' to '/lostgame'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Navigate to='/lostgame' />} />
+        <Route path='/lostgame' element={<Home />} />
         {/* <Route path='/about' element={<About />} /> */}
         {/* <Route path='*' element={<NotFound />} /> */}
       </Routes>
